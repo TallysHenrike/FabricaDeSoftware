9d07045c3dce9a1b93d620045a6c2864aca241ee
TallysHenrike <tallys.henrike@gmail.com>
2019-05-08 20:13:24 -0300

    Removido importações não utilizadas

--- src/main/java/br/com/fatesg/eventos/entities/Evento.java
+++ src/main/java/br/com/fatesg/eventos/entities/Evento.java
@@ -3,7 +3,6 @@ package br.com.fatesg.eventos.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
