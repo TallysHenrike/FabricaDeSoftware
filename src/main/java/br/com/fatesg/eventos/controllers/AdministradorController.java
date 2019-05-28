916c24de07a567801106bd33256532c48e7e9230
Franzwagner Ternus <franzwagner.str@gmail.com>
2019-05-27 21:12:27 -0300

    Adicionado ao front-end mensagens em caso de erro

--- src/main/java/br/com/fatesg/eventos/controllers/AdministradorController.java
+++ src/main/java/br/com/fatesg/eventos/controllers/AdministradorController.java
@@ -1,18 +1,16 @@
 package br.com.fatesg.eventos.controllers;
 
+import br.com.fatesg.eventos.entities.Administrador;
+import br.com.fatesg.eventos.persistence.AdministradorPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.fatesg.eventos.entities.Administrador;
-import br.com.fatesg.eventos.persistence.AdministradorPersistence;
 
 @RestController
 @RequestMapping("/restrito/administrador")
@@ -22,29 +20,58 @@ public class AdministradorController {
 	private AdministradorPersistence administradorDao;
 
 	@RequestMapping(value = "listar", method = RequestMethod.GET)
-	public List<Administrador> listar() {
-		return administradorDao.findAll();
+	public List<Administrador> listar(ServletResponse response) throws IOException {
+		try {
+			return administradorDao.findAll();
+		} catch (Exception e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível listar os administradores!");
+		}
+		return null;
 	}
 
 	@RequestMapping(value = "buscar/{id}", method = RequestMethod.GET)
-	public Optional<Administrador> buscar(@PathVariable Long id) {
-		return administradorDao.findById(id);
+	public Optional<Administrador> buscar(@PathVariable Long id, ServletResponse response) throws IOException {
+		try {
+			return administradorDao.findById(id);
+		} catch (Exception e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível buscar o administrador!");
+		}
+		return null;
 	}
 
 	@RequestMapping(value = "deletar/{id}", method = RequestMethod.DELETE)
-	public void deletar(@PathVariable Long id) {
-		administradorDao.deleteById(id);
+	public void deletar(@PathVariable Long id, ServletResponse response) throws IOException {
+		try {
+			administradorDao.deleteById(id);
+		} catch (Exception e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível deletar o administrador!");
+		}
 	}
 
 	@RequestMapping(value = "inserir", method = RequestMethod.POST)
-	public Administrador inserir(@RequestBody Administrador administrador) {
-		administrador.setDataDeCadastro(new Date());
-		return administradorDao.save(administrador);
+	public Administrador inserir(@RequestBody Administrador administrador, ServletResponse response) throws IOException {
+		try {
+			administrador.setDataDeCadastro(new Date());
+			return administradorDao.save(administrador);
+		} catch (Exception e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível adicionar o administrador!");
+		}
+		return null;
 	}
 
 	@RequestMapping(value = "alterar", method = RequestMethod.PUT)
-	public Administrador alterar(@RequestBody Administrador administrador) {
-		administrador.setDataDeAtualizacao(new Date());
-		return administradorDao.save(administrador);
+	public Administrador alterar(@RequestBody Administrador administrador, ServletResponse response) throws IOException {
+		try {
+			administrador.setDataDeAtualizacao(new Date());
+			return administradorDao.save(administrador);
+		} catch (Exception e) {
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível alterar o administrador!");
+		}
+		return null;
 	}
 }
--- src/main/java/br/com/fatesg/eventos/controllers/CategoriaController.java
+++ src/main/java/br/com/fatesg/eventos/controllers/CategoriaController.java
@@ -1,22 +1,16 @@
 package br.com.fatesg.eventos.controllers;
 
+import br.com.fatesg.eventos.entities.Categoria;
+import br.com.fatesg.eventos.persistence.CategoriaPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.fatesg.eventos.entities.Categoria;
-import br.com.fatesg.eventos.persistence.CategoriaPersistence;
 
 @RestController
 @RequestMapping("/restrito/categoria")
@@ -30,7 +24,8 @@ public class CategoriaController {
 		try {
 			return categoriaDao.findAll();
 		} catch (Exception e) {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao listar");
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível listar as categorias!");
 		}
 		return null;
 	}
@@ -40,7 +35,8 @@ public class CategoriaController {
 		try {
 			return categoriaDao.findById(id);
 		} catch (Exception e) {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao buscar");
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível buscar a categorias");
 		}
 		return null;
 	}
@@ -50,7 +46,8 @@ public class CategoriaController {
 		try {
 			categoriaDao.deleteById(id);
 		} catch (Exception e) {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao deletar");
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível deletar a categoria!");
 		}
 	}
 
@@ -60,7 +57,8 @@ public class CategoriaController {
 			categoria.setDataDeCadastro(new Date());
 			return categoriaDao.save(categoria);
 		} catch (Exception e) {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao inserir");
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível adicionar a categoria!");
 		}
 		return null;
 	}
@@ -71,9 +69,9 @@ public class CategoriaController {
 			categoria.setDataDeAtualizacao(new Date());
 			return categoriaDao.save(categoria);
 		} catch (Exception e) {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao alterar");
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Não foi " +
+					"possível alterar a categoria!");
 		}
 		return null;
 	}
-
 }
